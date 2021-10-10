7d81d28d4c4442ee35f39df1052ec8ad807c08dc
devsquid92 <johnm.ramirez@gmail.com>
2021-10-10 23:03:17 +1300

    Update features

--- JobSearchWebApp/Controllers/AccountController.cs
+++ JobSearchWebApp/Controllers/AccountController.cs
@@ -67,18 +67,36 @@ namespace JobSearchWebApp.Controllers
             return View();
         }
 
+        //public ActionResult LoggedIn()
+        //{
+        //    if (Session["UserId"] != null)
+        //    {
+
+        //        JobSearchDBEntities1 db = new JobSearchDBEntities1();
+
+        //            var userId = Convert.ToInt32(Session["UserId"]);
+        //            var dets = db.SavedJobs.Where(i => i.UserId == userId);
+
+        //            return View(dets.ToList());
+
+        //    }
+
+        //    else
+        //    {
+        //        return RedirectToAction("Login");
+        //    }
+        //}
+
         public ActionResult LoggedIn()
         {
             if (Session["UserId"] != null)
             {
+                var userId = Convert.ToInt32(Session["UserId"]);
 
-                JobSearchDBEntities1 db = new JobSearchDBEntities1();
-                
-                    var userId = Convert.ToInt32(Session["UserId"]);
-                    var dets = db.SavedJobs.Where(i => i.UserId == userId);
+                ProfileModel profileModel = new ProfileModel(userId);
 
-                    return View(dets.ToList());
-                
+                return View(profileModel);
+
             }
 
             else
@@ -89,18 +107,89 @@ namespace JobSearchWebApp.Controllers
 
 
 
-        public ActionResult Details()
-        {
-            
-
-            return View();
-        }
-
-
         public ActionResult Logout()
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Index", "Home");
         }
+
+        public ActionResult del(int sjobId)
+        {
+            if (ModelState.IsValid)
+            {
+                JobSearchDBEntities1 db = new JobSearchDBEntities1();
+                SavedJob savejob = new SavedJob();
+
+                var saveJobId = db.SavedJobs.Find(sjobId);
+                db.SavedJobs.Remove(saveJobId);
+                db.SaveChanges();
+
+                ViewBag.Message = "Saved Job Id: " + savejob.JobsId + " is successfuly deleted from your profile.";
+                return View();
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+            if (ModelState.IsValid)
+            {
+                JobSearchDBEntities1 db = new JobSearchDBEntities1();
+                User user = db.Users.Single(i => i.UserId == id);
+
+                return View(user);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            try
+            {
+                var userId = Convert.ToInt32(Session["UserId"]);
+
+
+                JobSearchDBEntities1 db = new JobSearchDBEntities1();
+                //var edit = db.Users.Where(i => i.UserId == userId).FirstOrDefault();
+                var edit = db.Users.Single(i => i.UserId == userId);
+                edit.FName = user.FName;
+                edit.LName = user.LName;
+                edit.Email = user.Email;
+                edit.Phone = user.Phone;
+                edit.DOB = user.DOB;
+                edit.Password = user.Password;
+                edit.ConfirmPassword = user.ConfirmPassword;
+                db.SaveChanges();
+
+
+                return RedirectToAction("LoggedIn");
+            }
+            catch(System.Data.Entity.Validation.DbEntityValidationException dbEx)
+            {
+                Exception raise = dbEx;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        string message = string.Format("{0}:{1}",
+                            validationErrors.Entry.Entity.ToString(),
+                            validationError.ErrorMessage);
+                        // raise a new exception nesting  
+                        // the current instance as InnerException  
+                        raise = new InvalidOperationException(message, raise);
+                    }
+                }
+                throw raise;
+            }
+
+
+        }
+
+
     }
 }
---
+++ JobSearchWebApp/Models/ProfileModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSearchWebApp.Models
+{
+    public class ProfileModel
+    {
+        public List<User> userdetails { get; set; }
+        public List<SavedJob> savedjob { get; set; }
+        //public int loggedUser { get; set; }
+
+        public ProfileModel(int loggedUser)
+        {
+            JobSearchDBEntities1 db = new JobSearchDBEntities1();
+
+            userdetails = db.Users.Where(i => i.UserId == loggedUser).ToList();
+            savedjob = db.SavedJobs.Where(i => i.UserId == loggedUser).ToList();
+        }
+    }
+}
